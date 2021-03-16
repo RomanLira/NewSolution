5a796bf51c9f61d339773e8a3cd1b84c9b63b329
RomanLira <lirca777@gmail.com>
2021-03-16 21:33:57 +0200

    List & Dictionary

---
+++ Tests/DictionaryTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Objects_Library;
+
+namespace Tests
+{
+    [TestClass]
+    public class DictionaryTest
+    {
+        private PersonGenerator generator = new PersonGenerator();
+
+        [TestMethod]
+        public void ValueAvailableForKeyTest()
+        {
+            var dictionary = generator.WorkPlaceDictionary(10);
+            var person = generator.GeneratePerson();
+            Console.WriteLine("Person for search: {0}", person.ToString());
+            string value = "";
+            if (dictionary.TryGetValue(person, out value))
+            {
+                Console.WriteLine("Person works in " + value);
+                Assert.IsTrue(dictionary.ContainsValue(value));
+            }
+            else
+            {
+                Console.WriteLine("Person not found");
+                Assert.IsFalse(dictionary.ContainsValue(value));
+            }
+        }
+    }
+}
