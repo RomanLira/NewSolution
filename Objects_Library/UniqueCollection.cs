9569f2abc39307c5735b0b70380c0b816405ef16
RomanLira <lirca777@gmail.com>
2021-03-28 14:13:10 +0300

    Unique Collection

---
+++ Objects_Library/UniqueCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace Objects_Library
+{
+    public class UniqueCollection<T>
+    {
+        private Collection<T> _collection;
+
+        public UniqueCollection()
+        {
+            _collection = new Collection<T>();
+        }
+
+        public void Add(T element)
+        {
+            if (element == null)
+                throw new ArgumentNullException("Null cannot added in collection");
+            else
+            {
+                if (_collection.Contains(element))
+                    throw new ArgumentException(element + "is already in collection");
+                else
+                    _collection.Add(element);
+            }
+        }
+
+        public void Print()
+        {
+            foreach (T elem in _collection)
+            {
+                Console.WriteLine(elem);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Tests/UniqueCollectionTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Objects_Library;
+
+namespace Tests
+{
+    [TestClass]
+    public class UniqueCollectionTest
+    {
+        private PersonGenerator persongenerator = new PersonGenerator(); 
+
+        private bool AllItemsAreUnique<T>(UniqueCollection<T> _collection)
+        {
+            List<T> list = new List<T>();
+            foreach (var elem in list)
+            {
+                list.Add(elem);
+                if (list.Contains(elem))
+                {
+                    Console.WriteLine("Collection is not unique");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        [TestMethod]
+        public void GenerateCollectionTest()
+        {
+            UniqueCollection<Person> _collection = new UniqueCollection<Person>();
+            for (int i = 0; i < 100; i++)
+            {
+                _collection.Add(persongenerator.GeneratePerson());
+            }
+            _collection.Print();
+            Assert.IsTrue(AllItemsAreUnique<Person>(_collection));
+        }
+    }
+}
