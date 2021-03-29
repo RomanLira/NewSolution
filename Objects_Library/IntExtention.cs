550e72ac58f15eb4438eb7147bf414da24b5d259
RomanLira <lirca777@gmail.com>
2021-03-29 16:27:53 +0300

    Extensions

---
+++ Objects_Library/IntExtention.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects_Library
+{
+    public static class IntExtention
+    {
+        public static TimeSpan MilliSeconds(this int value)
+        {
+            return new TimeSpan(0, 0, 0, 0, Math.Abs(value));
+        }
+
+        public static TimeSpan Seconds (this int value)
+        {
+            return new TimeSpan(0, 0, 0, Math.Abs(value), 0);
+        }
+
+        public static TimeSpan Minutes (this int value)
+        {
+            return new TimeSpan(0, 0, Math.Abs(value), 0, 0);
+        }
+
+        public static TimeSpan Hours (this int value)
+        {
+            return new TimeSpan(0, Math.Abs(value), 0, 0, 0);
+        }
+
+        public static TimeSpan Days (this int value)
+        {
+            return new TimeSpan(Math.Abs(value), 0, 0, 0, 0);
+        }
+    }
+}
---
+++ Tests/ExtensionTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Objects_Library;
+
+namespace Tests
+{
+    [TestClass]
+    public class ExtensionTest
+    {
+        private enum TimeMethod
+        {
+            MilliSeconds, Seconds, Minutes, Hours, Days
+        }
+        private TimeSpan TimeGenerator (TimeMethod time)
+        {
+            Random rand = new Random();
+            int value = rand.Next(0, 100000);
+            Console.WriteLine(value);
+            switch(time)
+            {
+                case TimeMethod.MilliSeconds: return value.MilliSeconds();
+                case TimeMethod.Seconds: return value.Seconds();
+                case TimeMethod.Minutes: return value.Minutes();
+                case TimeMethod.Hours: return value.Hours();
+                case TimeMethod.Days: return value.Days();
+                default: return 0.MilliSeconds();
+            }
+        }
+
+        [TestMethod]
+        public void IntValueToMilliSeconds()
+        {
+            TimeSpan t = TimeGenerator(TimeMethod.MilliSeconds);
+            Console.WriteLine(t);
+            Assert.IsNotNull(t);
+        }
+
+        [TestMethod]
+        public void IntValueToSeconds()
+        {
+            TimeSpan t = TimeGenerator(TimeMethod.Seconds);
+            Console.WriteLine(t);
+            Assert.IsNotNull(t);
+        }
+
+        [TestMethod]
+        public void IntValueToMinutes()
+        {
+            TimeSpan t = TimeGenerator(TimeMethod.Minutes);
+            Console.WriteLine(t);
+            Assert.IsNotNull(t);
+        }
+
+        [TestMethod]
+        public void IntValueToHours()
+        {
+            TimeSpan t = TimeGenerator(TimeMethod.Hours);
+            Console.WriteLine(t);
+            Assert.IsNotNull(t);
+        }
+
+        [TestMethod]
+        public void IntValueToDays()
+        {
+            TimeSpan t = TimeGenerator(TimeMethod.Days);
+            Console.WriteLine(t);
+            Assert.IsNotNull(t);
+        }
+    }
+}
